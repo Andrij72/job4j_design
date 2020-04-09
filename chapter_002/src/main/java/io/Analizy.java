73e73de50cfc7232753a49b2b82d378448e3aad2
Andrij72 <akulmmm@gmail.com>
2020-04-09 21:49:56 +0300

    fix Server availability analysis. [# 254015]

--- chapter_002/src/main/java/io/Analizy.java
+++ chapter_002/src/main/java/io/Analizy.java
@@ -9,16 +9,7 @@ public class Analizy {
     public static void unavailable(String source, String target) {
         StringJoiner out = new StringJoiner(System.lineSeparator());
         try (BufferedReader buf = new BufferedReader(new FileReader(source))) {
-            /* buf.lines()
-             *//* .filter(e -> e.substring(0, 2)). e.startsWith("500") || e.startsWith("400"))*//*
-                    .dropWhile(e -> e.startsWith("500") || e.startsWith("400"))
-                    .map(str -> "Start: " + str)
-                    .takeWhile(e -> e.startsWith("200") || e.startsWith("300"))
-                    .map(str -> "Finish: " + str)
-                    .forEach(out::add);*/
-            //  System.out.println(out.toString());
             Map<String, String> values = new LinkedHashMap<>();
-            Map<String, String> reslt = new LinkedHashMap<>();
             buf.lines().filter(l -> !l.isBlank())
                     .collect(Collectors.toMap(k -> k.substring(4), v -> v.substring(0, 3),
                             (a, b) -> String.join(", ", a, b),
@@ -33,7 +24,6 @@ public class Analizy {
                 if ((me.getValue().equals("500") || me.getValue().equals("400")) && !check) {
                     check = true;
                     out.add("Start server: " + me.getKey() + "\n");
-                    continue;
                 }
                 if ((me.getValue().equals("200") || me.getValue().equals("300")) && check) {
                     out.add("Finish server: " + me.getKey() + "\n");
