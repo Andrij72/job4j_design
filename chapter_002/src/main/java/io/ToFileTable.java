cb1e75bfc2fcb57e4a69eff291ca3eb3153e414a
Andrij72 <akulmmm@gmail.com>
2020-04-11 16:40:01 +0300

    System IO[#254010]

--- chapter_002/src/main/java/io/ToFileTable.java
+++ chapter_002/src/main/java/io/ToFileTable.java
@@ -14,10 +14,10 @@ public class ToFileTable {
     }
 
     public static void main(String[] args) {
-        StringBuilder builder = new StringBuilder("Demo write to file:");
-        builder.append(System.lineSeparator());
-        String buf = Stream.of("###Multiply table###", "1 * 2 = 2", "1 * 3 = 3", "1 * 4 = 4", "1 * 5 = 5", "1 * 6 = 6",
-                "1 * 7 = 7", "1 * 8 = 8", "1 * 9 = 9", "1 * 10 = 10", "###########").map(e -> e + "\n").collect(Collectors.joining());
+        StringBuilder builder = new StringBuilder();
+        String buf = Stream.of("Demo write to file:", "###Multiply table###", "1 * 2 = 2", "1 * 3 = 3", "1 * 4 = 4",
+                              "1 * 5 = 5", "1 * 6 = 6", "1 * 7 = 7", "1 * 8 = 8", "1 * 9 = 9", "1 * 10 = 10")
+                            .collect(Collectors.joining(System.lineSeparator()));
         builder.append(buf);
         writeToFile(builder.toString());
     }
