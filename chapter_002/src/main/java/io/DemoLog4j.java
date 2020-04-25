4d24f6f24af08cc5761f6b951d2b3ded618a0e8c
Andrij72 <akulmmm@gmail.com>
2020-04-26 00:12:04 +0300

    3. Slf4j - [#268852]

---
+++ chapter_002/src/main/java/io/DemoLog4j.java
@@ -0,0 +1,20 @@
+package io;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DemoLog4j {
+    private static final Logger LOG = LoggerFactory.getLogger(DemoLog4j.class.getName());
+
+    public static void main(String[] args) {
+        byte a = 4;
+        short b = 44;
+        int c = 444;
+        Long d = -44442L;
+        float e = 4.4F;
+        boolean f = true;
+        boolean g = false;
+        char h = 40;
+        LOG.debug("Var a:{}, Var b:{}, Var c:{}, Var d:{}, Var e:{}, Var f:{}, Var g:{}, Var h:{}", a, b, c, d, e, f, g, h);
+    }
+}
