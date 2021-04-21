cce38c1f6e92bf19e0a7519dc7c849a303d68e39
Andrij72 <akulmmm@gmail.com>
2021-04-21 17:00:38 +0300

    create even numbers iterator

---
+++ chapter_003/src/main/java/it/eventit/EvenNumIterator.java
@@ -0,0 +1,32 @@
+package it.eventit;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumIterator implements Iterator<Integer> {
+    private final int[] massiveNumbers;
+    private int elem = 0;
+
+    public EvenNumIterator(int[] numbers) {
+        this.massiveNumbers = numbers;
+    }
+
+    public Integer next() {
+        if (!hasNext()) {
+           throw new NoSuchElementException();
+        }
+        return massiveNumbers[elem++];
+    }
+
+    public boolean hasNext() {
+        boolean res = false;
+        while (elem < massiveNumbers.length) {
+            if (massiveNumbers[elem] % 2 == 0) {
+                res = true;
+                break;
+            }
+            elem++;
+        }
+        return res;
+    }
+}
