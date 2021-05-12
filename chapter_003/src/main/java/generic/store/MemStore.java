b398e837212f11c69eb1977424b1bf018aa36f58
Andrij72 <akulmmm@gmail.com>
2021-05-12 16:08:24 +0300

    realizing Store <T extends Base>

---
+++ chapter_003/src/main/java/generic/store/MemStore.java
@@ -0,0 +1,23 @@
+package generic.store;
+
+public class MemStore<T extends Base> implements Store<T> {
+    @Override
+    public void add(T model) {
+
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
+
+    @Override
+    public T findById(String id) {
+        return null;
+    }
+}
---
+++ chapter_003/src/main/java/generic/store/Role.java
@@ -0,0 +1,7 @@
+package generic.store;
+
+public class Role extends Base {
+    public Role(String id) {
+        super(id);
+    }
+}
