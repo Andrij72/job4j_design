8dcec8e7c60e3bab3447ddef02d3bc161b742e6e
Andrij72 <akulmmm@gmail.com>
2021-06-07 16:35:07 +0300

    modifies SimpleSet<T>

--- chapter_003/src/main/java/set/SimpleSet.java
+++ chapter_003/src/main/java/set/SimpleSet.java
@@ -3,9 +3,25 @@ package set;
 import list.SimpleArray;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 public class SimpleSet<T> implements SetI<T> {
     SimpleArray<T> arLists = new SimpleArray<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SimpleSet<?> simpleSet = (SimpleSet<?>) o;
+
+        return Objects.equals(arLists, simpleSet.arLists);
+    }
+
+    @Override
+    public int hashCode() {
+        return arLists != null ? arLists.hashCode() : 0;
+    }
 
     @Override
     public boolean add(T value) {
