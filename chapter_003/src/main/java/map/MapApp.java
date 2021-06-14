a012ae49ced8949b3d169a4fe9e67f1117226e78
Andrij72 <akulmmm@gmail.com>
2021-06-14 12:07:05 +0300

    User model map

---
+++ chapter_003/src/main/java/map/MapApp.java
@@ -0,0 +1,15 @@
+package map;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapApp {
+    public static void main(String[] args) {
+        User usr1 = new User("Roman", 2, new GregorianCalendar(1997, 12, 21));
+        User usr2 = new User("Petr", 4, new GregorianCalendar(1995, 5, 9));
+        Map<User, Object> mp = new HashMap<>();
+        mp.put(usr1, new Object());
+        mp.put(usr2, new Object());
+    }
+}
