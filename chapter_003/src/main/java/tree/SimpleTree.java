348846889617ec5084aa4b8fdc305fb3b047a6ee
Andrij72 <akulmmm@gmail.com>
2021-07-05 22:28:37 +0300

    modifier SimpleTree with Predicate

--- chapter_003/src/main/java/tree/SimpleTree.java
+++ chapter_003/src/main/java/tree/SimpleTree.java
@@ -1,8 +1,11 @@
 package tree;
 
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Queue;
+import java.util.function.Predicate;
+
 
 public class SimpleTree<E> implements Tree<E> {
     private Node<E> root;
@@ -23,6 +26,8 @@ public class SimpleTree<E> implements Tree<E> {
         return false;
     }
 
+
+    /* In next refactoring this method with Predicate<Node<E>> condition.
     @Override
     public Optional<Node<E>> findBy(E value) {
         Optional<Node<E>> rsl = Optional.empty();
@@ -30,7 +35,29 @@ public class SimpleTree<E> implements Tree<E> {
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.getValue().equals(value)) {
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+     */
+
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        return findByPredicate(eNode -> Objects.equals(eNode.getValue(), value));
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        Node<E> el = data.poll();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
@@ -38,4 +65,9 @@ public class SimpleTree<E> implements Tree<E> {
         }
         return rsl;
     }
+
+    public boolean isBinary() {
+        return findByPredicate(eNode -> eNode.getChildren().size() > 2).isPresent();
+    }
+
 }
