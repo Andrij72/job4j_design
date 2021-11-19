20b7cd6a2d9fa7b48c19a237f1cf6cf8adab4cab
Andrij72 <akulmmm@gmail.com>
2021-11-19 14:54:29 +0200

    add test method load fo ImportDb

--- chapter_004/src/test/java/ru/job4/spammer/ImportDbTest.java
+++ chapter_004/src/test/java/ru/job4/spammer/ImportDbTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -19,4 +20,14 @@ public class ImportDbTest {
         assertThat(usrs.get(0).getName(), is("Ivan Ivanov"));
         assertThat(usrs.get(0).getEmail(), is("iivanov@gmail.com"));
     }
+
+    @Test
+    public void load() throws SQLException, IOException, ClassNotFoundException {
+        List<User> exp = new ImportDb("./src/test/dump.txt").load();
+        List<User> expect = exp.stream().filter(user ->
+                user.getName().equals("Ivan Ivanov") && user.getEmail().equals("iivanov@gmail.com")).collect(Collectors.toList());
+        assertThat(expect.size(), is(1));
+    }
+
+
 }
